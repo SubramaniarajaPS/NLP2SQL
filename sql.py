7eedeb11f0ea8594aa82cad0ff0a7d5b59ec9e2c
Subramaniaraja PS <69624562+SubramaniarajaPS@users.noreply.github.com>
2024-02-23 20:37:55 +0530

    Master - Building Local SQL DB

---
+++ sql.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+# Connection to sqlite
+connection= sqlite3.connect("map.db")
+
+# Create an object of cursor to perform CRUD operation
+cursor = connection.cursor()
+
+#Create the DB Table
+table_info = """
+Create table MAP(WARNING VARCHAR(6),DTC VARCHAR(4),FAILURE VARCHAR(2), CAUTION VARCHAR(100),SEVERITY INT)
+"""
+cursor.execute(table_info)
+
+#Insert records into the table
+cursor.execute('''Insert Into MAP values('600E00','0000','00','Start Engine Alert', 1) ''')
+cursor.execute('''Insert Into MAP values('600E01','0001','01','Turn ON AC', 1) ''')
+cursor.execute('''Insert Into MAP values('600E02','0002','02','Check Side Indicator', 1) ''')
+cursor.execute('''Insert Into MAP values('600E03','0003','03','Turn On Headlight', 1) ''')
+cursor.execute('''Insert Into MAP values('600E10','0010','10','Oil Life Alert', 2) ''')
+cursor.execute('''Insert Into MAP values('600E11','0011','11','Tyre Slow Leak Alert', 2) ''')
+cursor.execute('''Insert Into MAP values('600E12','0012','12','Door Ajar Alert', 2) ''')
+cursor.execute('''Insert Into MAP values('600E13','0013','13','Bonnet Ajar Alert', 2) ''')
+cursor.execute('''Insert Into MAP values('600E20','0020','20','Vehicle Engine Damage ALert', 3) ''')
+cursor.execute('''Insert Into MAP values('600E21','0021','21','Drive Belt Distortion Alert', 3) ''')
+cursor.execute('''Insert Into MAP values('600E22','0022','22','Steering Wheel Malfunction Alert', 3) ''')
+cursor.execute('''Insert Into MAP values('600E23','0023','23','Battery Dead Alert', 3) ''')
+
+#Displaying all the records
+print("The inserted records are")
+data = cursor.execute('''Select * From MAP''')
+for row in data:
+  print(row)
+
+#Close the connection
+connection.commit()
+connection.close()
